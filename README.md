c804904e741c0f1405ebe6e8ec9fdf4b28bb771a
Anna Tuhusova <bilachayka@mail.com>
2025-08-18 16:31:57 +0200

    2048

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# 2048 Game
+
+A classic 2048 puzzle game built for the web. Combine tiles with the same numbers to reach the elusive 2048 tile. Built with a focus on smooth gameplay, responsive design, and simple, intuitive controls.
+
+I believe that games should be interesting, exciting, and accessible — without unnecessary details, without distractions, just pure puzzle enjoyment.
+
+Challenge yourself and enjoy the addictive sliding puzzle experience.
+
+[DEMO LINK](https://tuhusova.github.io/2048-game/)
+
+## Technologies Used:
+- HTML5
+- SCSS
+- JavaScript (ES6)
+
+## How to set up the project locally:
+  - Clone the repository:
+**git clone**
+  - **cd project-name**
+  - Install dependencies:
+**npm install**
+or
+**yarn install**
+  - Run the project locally:
+**npm start**
+or
+**yarn start**
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,76 @@
 'use strict';
+/* global confirm */
+
+const Game = require('../modules/Game.class');
+const game = new Game();
+
+const button = document.querySelector('.button');
+
+const messageContainer = document.querySelector('.message-container');
+const messageLose = document.querySelector('.message-lose');
+const messageWin = document.querySelector('.message-win');
+const messageStart = document.querySelector('.message-start');
+
+function showMessage(messageType) {
+  messageContainer.classList.remove('hidden');
+
+  messageLose.classList.add('hidden');
+  messageWin.classList.add('hidden');
+  messageStart.classList.add('hidden');
+
+  if (messageType === 'lose') {
+    messageLose.classList.remove('hidden');
+  } else if (messageType === 'win') {
+    messageWin.classList.remove('hidden');
+  } else if (messageType === 'start') {
+    messageStart.classList.remove('hidden');
+  }
+}
+
+button.addEventListener('click', () => {
+  if (game.status === 'idle') {
+    game.start();
+    showMessage('start');
+
+    button.classList.remove('start');
+    button.classList.add('restart');
+    button.textContent = 'Restart';
+    messageStart.textContent = 'Press "Restart" to start the new Game';
+  } else {
+    const confirmRestart = confirm('Do you want to start the new game?');
+
+    if (confirmRestart) {
+      game.restart();
+      game.start();
+      showMessage('start');
+      button.textContent = 'Restart';
+    }
+  }
+});
+
+document.addEventListener('keydown', (ev) => {
+  if (game.status === 'playing') {
+    switch (ev.key) {
+      case 'ArrowLeft':
+        game.moveLeft();
+        break;
+      case 'ArrowRight':
+        game.moveRight();
+        break;
+      case 'ArrowUp':
+        game.moveUp();
+        break;
+      case 'ArrowDown':
+        game.moveDown();
+        break;
+      default:
+        break;
+    }
+  }
+
+  if (game.isGameOver()) {
+    showMessage('lose');
+  } else if (game.status === 'win') {
+    showMessage('win');
+  }
+});
